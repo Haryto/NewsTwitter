336048dda3c78279b998acfe86e3b8c8933ebf50
Haryto <37344295+Haryto@users.noreply.github.com>
2018-05-05 00:12:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<link href="css/styles.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
