0ed7cca5a1ed68c869e8f49dd1511901faef0b1a
Haryto <gatesofbaldur@gmail.com>
2018-05-04 17:14:40 +0300

    Test commit after merging

--- ajax_handler.php
+++ ajax_handler.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 require 'connection.php';
@@ -21,28 +20,4 @@ if ( isset( $_POST['latestId'] ) && is_numeric( $_POST['latestId'] ) && $_POST['
 }
 elseif ( isset( $_POST['timestamp'] ) && is_numeric( $_POST['timestamp'] ) && $_POST['action'] === 'updateDate' ) {
 	echo twitter_time( $_POST['timestamp'], true );
-=======
-<?php
-
-require 'connection.php';
-
-if ( isset( $_POST['latestId'] ) && is_numeric( $_POST['latestId'] ) && $_POST['action'] === 'updateTweets' ) {
-
-	$latestId = $_POST['latestId'];
-
-	try {
-		$last_tweets = $connection->get( 'statuses/user_timeline', array( 'screen_name' => $user_name, 'since_id' => $latestId, 'trim_user' => false ) );
-		if ( !isset( $last_tweets->errors ) ) 	
-			echo wrapTweets( $last_tweets, true );
-		else {
-			logError( print_r( $last_tweets, true ) );
-		}
-	}
-	catch( Abraham\TwitterOAuth\TwitterOAuthException $e ) {
-		logError( 'Выброшено исключение: ' .  $e->getMessage() . "\n" );
-	}
-}
-elseif ( isset( $_POST['timestamp'] ) && is_numeric( $_POST['timestamp'] ) && $_POST['action'] === 'updateDate' ) {
-	echo twitter_time( $_POST['timestamp'], true );
->>>>>>> 0d742b84ba88c9eea1c84181da7dda098bd2e556
 }
--- connection.php
+++ connection.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 include 'helpers.php';
@@ -16,23 +15,4 @@ require "vendor/autoload.php";
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-=======
-<?php
-
-include 'helpers.php';
-
-define( 'CONSUMER_KEY', 'oUb0QpOVL42iY8HziGNCAZhnG' );
-define( 'CONSUMER_SECRET', 'cbjcWDsMlkzX07PoIf8wEbgJBiUNgO073xtnCbGtrnLPeGqwvi' );
-define( 'OAUTH_TOKEN', '991697977956847616-1EWKim71EImkV3NTbq7uGiTSV3REOyI' );
-define( 'OAUTH_SECRET', 'gFfyGMUIT7z0wsgGsclfYMWIFYkVa7GNG6LGnHRdAHxk7' );
-
-$user_name = 'AKulbii';
-//$user_name = 'BarackObama';
-$count_twits = 25;
-
-require "vendor/autoload.php";
-
-use Abraham\TwitterOAuth\TwitterOAuth;
-
->>>>>>> 0d742b84ba88c9eea1c84181da7dda098bd2e556
 $connection = new TwitterOAuth( CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_SECRET );
